e670bb105852116a2af84d3a212a32654f749285
tznate <i.am.nathan.boy@gmail.com>
2023-03-23 23:37:29 +0530

    Project done

---
+++ app/src/main/java/com/example/shoplist/GroceryDao.kt
@@ -0,0 +1,18 @@
+package com.example.shoplist
+
+import androidx.lifecycle.LiveData
+import androidx.room. Dao
+import androidx.room. Delete
+import androidx.room. Insert
+import androidx.room. OnConflictStrategy
+import androidx.room.Query
+
+@Dao
+interface GroceryDao {
+    @Insert (onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(item: GroceryItems)
+    @Delete
+    suspend fun delete(item: GroceryItems)
+    @Query("SELECT * FROM grocerry_items")
+    fun getAllGroceryItems (): LiveData<List<GroceryItems>>
+}
---
+++ app/src/main/java/com/example/shoplist/GroceryRepository.kt
@@ -0,0 +1,7 @@
+package com.example.shoplist
+
+class GroceryRepository (private val db: GroceryDatabase) {
+    suspend fun insert(items: GroceryItems) = db.getGroceryDao().insert(items)
+    suspend fun delete(items: GroceryItems) = db.getGroceryDao().delete(items)
+    fun getAllItems () = db.getGroceryDao().getAllGroceryItems()
+}
